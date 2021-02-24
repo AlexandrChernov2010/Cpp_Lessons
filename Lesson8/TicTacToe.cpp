593d6408c636bbaf44c53b2cead1552901c99d01
Alexandr <chernovalexandr2010@bk.ru>
2021-02-24 23:58:40 +0400

    Another file

--- Lesson8/TicTacToe.cpp
+++ Lesson8/TicTacToe.cpp
@@ -2,7 +2,7 @@
 #include <random>
 #include <algorithm>
 #include <windows.h>
-#include <locale>
+//#include <locale>
 
 #define CHECK_DOT(X,Z) (((X) >= 0) && ((X) < (Z)))
 #define POINT_ITEM(a,r,c) (*((*((a) + (r))) + (c)))
@@ -10,7 +10,7 @@
 //C++11 <random>
 std::random_device rd;
 std::mt19937 mt(rd());
-std::uniform_int_distribution<int> dist(0, 6);
+//std::uniform_int_distribution<int> dist(0, *p_interval);
 
 enum PLAYER { HUMAN='X', AI='O', EMPTY='_' };
 typedef struct {
@@ -26,10 +26,37 @@ void setVal(PLAYER **array, const int row, const int col, PLAYER value) {
 	POINT_ITEM(array, row, col) = value;
 }
 
+void Request (Field &field) {
+    char Answer;
+    while (!Answer)
+    {
+        std::cout << "Do you want to change the size of the field or leave it as default (y/n) ";
+        std::cin >> Answer;
+    }
+    switch (Answer)
+    {
+        case('y'):
+            std::cout << "Enter the size of the field ";
+            std::cin >> field.szX;
+            field.szY = field.szX;
+            std::cout << "Enter the size to win ";
+            std::cin >> field.toWin;
+            break;
+        case('n'):
+            field.toWin = 5;
+            field.szX = 7;
+            field.szY = 7;
+            break;
+
+
+    }
+
+}
 void init(Field &field) {
-	field.toWin = 5;
-	field.szX = 7;
-	field.szY = 7;
+	//field.toWin = 5;
+	//field.szX = 7;
+	//field.szY = 7;
+	Request(field);
 	field.map = (PLAYER **) calloc(field.szX, sizeof(PLAYER *));
 	for (int y = 0; y < field.szY; y++) {
 		*(field.map + y) = (PLAYER *) calloc(field.szX, sizeof(PLAYER));
@@ -42,7 +69,11 @@ void print(Field &field) {
 	//#include <windows.h>
 	std::system("cls");
 //	std::system("clear");
-	std::cout << "-1-2-3-4-5-6-7-\n";
+    for (int i = 1; i <= field.szX; i++) {
+        std::cout << "-" << i;
+    }
+    std::cout << "-\n";
+	//std::cout << "-1-2-3-4-5-6-7-\n";
 	for (int y = 0; y < field.szY; y++) {
 		std::cout << (y + 1);
 		for (int x = 0; x < field.szX; x++) {
@@ -141,6 +172,7 @@ bool humWinCheck(Field &field) {
 
 
 void aiTurn(Field &field) {
+    std::uniform_int_distribution<int> dist(0, field.szX - 1);
 	if (aiWinCheck(field)) return;
 	if (humWinCheck(field)) return;
 	int x;
